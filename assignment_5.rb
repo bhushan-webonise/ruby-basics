d7e1692802585e739c28cdde34da6ced118e476b
Kiran Webonise <86220936+Kiransai-webonise@users.noreply.github.com>
2023-02-24 18:22:30 +0530

    Revert "string_manipulation | count_of_occurance"

--- assignment_5.rb
+++
@@ -1,6 +0,0 @@
-str1 = "RUBY parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as RUBY code. The parser then executes all of the code it finds until it runs into a RUBY closing <br/> tag."
-str2 = "RUBY does not require (or support) explicit type definition in variable declaration; a variable's type is determined by the context in which the variable is used."
-
-a = str1.scan('RUBY').count
-puts a
-
